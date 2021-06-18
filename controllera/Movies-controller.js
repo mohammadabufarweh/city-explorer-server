1635bc6cebe75a93cba3485d9b8109b70d8adc0c
Mohammadabufarweh <abufarweh34@gmail.com>
2021-06-19 00:42:40 +0300

    lab-10

--- controllera/Movies-controller.js
+++ controllera/Movies-controller.js
@@ -1,27 +1,44 @@
-  
+
 const axios = require('axios');
 const Movie = require('../models/Movies-Models');
+const Cache = require('../helper/cache')
+
 require('dotenv').config();
 const MOVIE_KEY = process.env.MOVIE_KEY;
+const cacheObjMovies = new Cache();
+
+
 const movieController = (request, ress) => {
-    let mov = request.query.query;
-    if (mov) {
+  let mov = request.query.query;
+  const requestKeyForMovies = `movies-${mov}`;
+
+  if (mov) {
+    if (cacheObjMovies[requestKeyForMovies] && (Date.now() - cacheObjMovies[requestKeyForMovies].timestamp < 86400000)) {
+
+      ress.send(cacheObjMovies[requestKeyForMovies]);
+
+    } else {
+
       const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_KEY}&query=${mov}`;
       axios.get(movieUrl).then(respomseMovie => {
-        const arr=[];
-  
-         respomseMovie.data.results.map(obj => {
+        const arr = [];
+
+        respomseMovie.data.results.map(obj => {
           let imageURL = `https://image.tmdb.org/t/p/w500${obj.poster_path}`;
-          let newMovie =new Movie (obj.title, obj.overview, obj.vote_average, obj.vote_count, imageURL, obj.popularity, obj.release_date);
+          let newMovie = new Movie(obj.title, obj.overview, obj.vote_average, obj.vote_count, imageURL, obj.popularity, obj.release_date);
           arr.push(newMovie);
-          });
+        });
+
+        cacheObjMovies[requestKeyForMovies] = arr;
+        cacheObjMovies[requestKeyForMovies].timestamp = Date.now();
         ress.send(arr);
-  
+
       }).catch(error => {
         ress.send(error.message)
       });
-    } else {
-      ress.send('please provide the proper movies')
     }
+  } else {
+    ress.send('please provide the proper movies')
+  }
 }
 module.exports = movieController;
--- controllera/weather-controllers.js
+++ controllera/weather-controllers.js
@@ -1,19 +1,31 @@
 const axios = require('axios');
 const Weather = require('../models/Weather-Models');
+const Cache = require('../helper/cache')
 require('dotenv').config();
+
+const cacheObj = new Cache();
+
 const WEATHER_BIT_KEY = process.env.WEATHER_BIT_KEY;
 const weatherController =  (req, res) => {
     const lat = req.query.lat;
     const lon = req.query.lon;
+    const requestKey =`weather-${lat}-${lon}`;
     if (lat && lon) {
-      const weatherBitUrl =`https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`;
-  
-      axios.get(weatherBitUrl).then(response => {
-        const responseData = response.data.data.map(obj => new Weather(obj));
-        res.json(responseData)
-      }).catch(error => {
-        res.send(error.message)
-      });
+      if( cacheObj[requestKey] && (Date.now() - cacheObj[requestKey].timestamp < 86400000)){
+        res.json(cacheObj[requestKey]);
+      }else{
+
+        const weatherBitUrl =`https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`;
+    
+        axios.get(weatherBitUrl).then(response => {
+          const responseData = response.data.data.map(obj => new Weather(obj));
+          cacheObj[requestKey]=responseData;
+          cacheObj[requestKey].timestamp = Date.now();
+          res.json(responseData);
+        }).catch(error => {
+          res.send(error.message)
+        });
+      }
     } else {
       res.send('please provide the proper lat and lon')
     }
